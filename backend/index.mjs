543f4af037b530d93298780a9bd9c4a6cdb8dbc6
Andrei Sucman <sucmanandrei@gmail.com>
2022-10-09 19:55:58 +0300

    test

--- backend/index.mjs
+++ backend/index.mjs
@@ -1,10 +1,10 @@
 import Fastify from "fastify";
 import * as cors from "@fastify/cors";
-// import { move, status, moves, fetchAiLevel, saveScore } from "./lib/js-chess-engine.mjs";
+import { move, status, moves, fetchAiLevel, saveScore } from "./lib/js-chess-engine.mjs";
 import { pay, sendToTelegram } from "./moralis.mjs";
 
-import jsChessEngine from "./js-chess-engine.js";
-const { move, status, moves, fetchAiLevel, saveScore } = jsChessEngine;
+// import jsChessEngine from "./js-chess-engine.js";
+// const { move, status, moves, fetchAiLevel, saveScore } = jsChessEngine;
 
 const ROUTE_MAP = {
   "/moves": moves,
