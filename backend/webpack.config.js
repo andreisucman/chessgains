9d01fd3caa585da2ce9ce4fbb5dacf6038be4e1d
Andrei Sucman <sucmanandrei@gmail.com>
2022-10-15 11:54:33 +0300

    module rule added

--- backend/webpack.config.js
+++ backend/webpack.config.js
@@ -1,33 +1,29 @@
 const path = require("path");
 
 module.exports = {
-  configureWebpack: {
-    module: {
-      rules: [
-        {
-          test: /\.mjs$/,
-          include: /node_modules/,
-          type: "javascript/auto",
-        },
-      ],
-    },
-    mode: "production",
-    entry: "./lib/js-chess-engine.mjs",
-    output: {
-      library: "js-chess-engine",
-      libraryTarget: "umd",
-      globalObject: "this",
-      umdNamedDefine: true,
-      filename: "js-chess-engine.js",
-    },
-    resolve: {
-      alias: {
-        "magic-sdk": path.resolve(__dirname, "node_modules/magic-sdk/dist/cjs/index.js"),
-      },
-    },
-    node: {
-      child_process: "empty",
-      fs: "empty",
+  mode: "production",
+  entry: "./lib/js-chess-engine.mjs",
+  output: {
+    library: "js-chess-engine",
+    libraryTarget: "umd",
+    globalObject: "this",
+    umdNamedDefine: true,
+    filename: "js-chess-engine.js",
+  },
+  resolve: {
+    alias: {
+      "magic-sdk": path.resolve(__dirname, "node_modules/magic-sdk/dist/cjs/index.js"),
     },
   },
+  node: {
+    child_process: "empty",
+    fs: "empty",
+  },
+  plugins: [
+    new webpack.LoaderOptionsPlugin({
+      test: /\.mjs$/,
+      include: /node_modules/,
+      type: "javascript/auto",
+    }),
+  ],
 };
