f0035e9438ddf20b82cfba632a96f2a146c0277d
Andrei Sucman <sucmanandrei@gmail.com>
2022-10-21 22:41:49 +0300

    tweaked disclaimer

--- frontend/components/GasDisclaimer.js
+++ frontend/components/GasDisclaimer.js
@@ -1,10 +1,9 @@
-import YoutubeEmbed from "./YoutubeIEmbed";
 import styles from "../styles/GasDisclaimer.module.scss";
 
 export default function GasDisclaimer() {
   return (
     <div className={styles.gas_disclaimer}>
-      <div className={styles.gas_disclaimer__ico}></div><span className={styles.gas_disclaimer__text}>The Polygon chain is overloaded. Transactions may take several minutes to process. To ensure the stability of your transactions always keep ~0.5 MATIC on your balance for covering the gas fees.</span>
+      <div className={styles.gas_disclaimer__ico}></div><span className={styles.gas_disclaimer__text}>The Polygon chain is overloaded. Transactions may take several minutes to process. To ensure the stability of your transactions keep ~0.5 more MATIC on your balance and wait patiently.</span>
     </div>
   );
 }
