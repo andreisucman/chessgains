60853e5c398775333ee1d5836328061ba05ab59f
Andrei Sucman <sucmanandrei@gmail.com>
2022-10-27 16:24:27 +0300

    changed about video

--- frontend/components/HowItWorks.js
+++ frontend/components/HowItWorks.js
@@ -5,7 +5,7 @@ export default function HowItWorks({ howItWorksRef }) {
   return (
     <div className={styles.how_it_works} ref={howItWorksRef}>
       <h2 className={styles.how_it_works__title}>How does it work?</h2>
-      <YoutubeEmbed videoId={"US50dMkLSCw"} title={"Chessgains - Skill-based competition for chess lovers"} />
+      <YoutubeEmbed videoId={"eq50ulkmUG4"} title={"Chessgains - Skill-based competition for chess lovers"} />
     </div>
   );
 }
