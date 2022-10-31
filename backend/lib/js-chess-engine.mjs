080fd24e2b47ee0b86b48d381e466f47dfdeb13e
Andrei Sucman <sucmanandrei@gmail.com>
2022-10-31 10:56:41 +0300

    reverted

--- backend/lib/js-chess-engine.mjs
+++ backend/lib/js-chess-engine.mjs
@@ -146,6 +146,7 @@ export async function getStockFishMove(config, level) {
     fen,
     depth: stockFishLevel,
     multipv: 1,
+    excludes: [PROVIDERS.LICHESS_BOOK, PROVIDERS.LICHESS_CLOUD_EVAL],
   });
 
   const allMoves = analysis.moves.map((entry) => entry.uci).flat(1);
