2a9108842a653f89946719642b62e5b580a80ff9
Andrei Sucman <sucmanandrei@gmail.com>
2022-11-01 10:19:45 +0300

    removed anticheat

--- frontend/components/GasDisclaimer.js
+++ frontend/components/GasDisclaimer.js
@@ -5,8 +5,7 @@ export default function GasDisclaimer() {
     <div className={styles.gas_disclaimer}>
       <div className={styles.gas_disclaimer__ico}></div>
       <span className={styles.gas_disclaimer__text}>
-        The chain is loaded. Transactions may take several minutes to process. To ensure the stability of your transactions keep
-        ~0.5 MATIC on your balance and wait patiently.
+        Transactions may take several minutes to process. Please always keep ~0.5 MATIC on your balance and wait patiently.
       </span>
     </div>
   );
